1940c047134ad2c6823a53f89473c0a831c1a1c3
yarslvd <91560707+yarslvd@users.noreply.github.com>
2022-07-06 13:57:23 +0300

    JavaScript Assignment

---
+++ module4-solution/script.js
@@ -0,0 +1,13 @@
+(function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (let i = 0; i < names.length; i++) {
+        let firstLetter = names[i].charAt().toLowerCase();
+
+        if (firstLetter == 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
